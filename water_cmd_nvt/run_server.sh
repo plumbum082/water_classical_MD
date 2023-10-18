9991cddae30c5fcd136e19946533141b9ac136d7
plumbum082 <54125554+plumbum082@users.noreply.github.com>
2023-10-18 13:45:40 +0800

    Add files via upload

---
+++ water_cmd_nvt/run_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#source /share/home/qiangao/installation/i-pi/env.sh
+
+export OMP_NUM_THREADS=1
+
+cat input.xml | sed -e "s/<address> \([a-zA-Z_]\+\)/<address> \1/" > .tmp.xml
+# i-pi simulation.restart >& logfile &
+i-pi .tmp.xml >& logfile &
+wait
+
+
+
+
+
